ed3739312c6eaa0d1172e39e83630367da1be98b
Ghost_23 <80656496+da260@users.noreply.github.com>
2025-02-01 21:32:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 Cliente = cliente
 Conta = conta
 
+# imprime na tela 
 print("*************************")
 print("BEM VINDO AO BANCO INTER ")
 print("*************************")
@@ -12,15 +13,20 @@ print()
 
 print("Vamos criar a sua conta: ")
 
+# entrada dos dados do cliente
 cliente.nome = input("Digite o seu nome: ")
 cliente.cpf = float(input("Digite seu cpf: "))
 cliente.idade = int(input("Digite sua idade: "))
 conta.limite = float(input("Digite um limite para sua conta: "))
 
+# imprime os dados 
 print("Nome: ", cliente.nome)
 print("CPF: ", cliente.cpf)
 print("Idade: ", cliente.idade)
 os.system("cls")
+
+# faz o saque / deposito na conta 
+# pode ser alterado no futuro e melhorado a lógica 
 
 print("DEPOSITAR, SACAR E VERIFICAR SALDO ")
 conta.depositar = float(input("Digite o valor a ser depositado: "))
