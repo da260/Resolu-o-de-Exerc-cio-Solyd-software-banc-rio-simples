3a041f764120d5cf7e9830a0f73ffe46e54da0a8
Ghost_23 <80656496+da260@users.noreply.github.com>
2025-02-01 21:29:03 -0300

    Update contas.py

--- contas.py
+++ contas.py
@@ -1,18 +1,22 @@
+# cria a conta do cliente
 class conta :
     def __init__(self, saldo, limite,):
         self.saldo = saldo
         self.limite = 0 - limite
-
+        
+# cria a função depositar 
     def depositar(self, quant):
         if quant > 0:
             self.saldo += quant
             print("Foi depositado: ", quant)
         else:
             print("Erro no deposito")    
-    
+
+# cria a função consulta saldo
     def consulta_saldo (self):
         return self.saldo
-    
+
+# cria a função sacar    
     def sacar(self, quant):
         if self.saldo - quant < self.limite:
             print("Saldo insuficiente")
